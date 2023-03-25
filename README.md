ba744de5c41a864ee6fdddaef62a57c967138c27
Aravind104 <ontagodiaravind@gmail.com>
2023-03-25 19:13:05 +0530

    use localStorage to persist the cart items after refresh

--- README.md
+++ README.md
@@ -3,3 +3,42 @@
 ## :page_facing_up: Description
 
 - mobile shop application helps users to add items to cart
+
+## 🚀 deployed on [netlify](https://app.netlify.com/)
+
+- [https://mobile-shop-cart-update.netlify.app/](https://mobile-shop-cart-update.netlify.app/)
+
+## 🛠 Installation and setup
+
+1. Clone the repo to your local machine.
+2. Install the required dependency for server using :
+
+   ```javascript
+   npm install
+   ```
+
+3. Start the dev server using :
+
+   ```javascript
+   npm run dev
+   ```
+
+## ⚙ Tools and Technologies used
+
+- [React.js](https://react.dev/)
+- [tailwind css](https://tailwindcss.com/)
+- [material ui icons](https://mui.com/material-ui/getting-started/installation/)
+- [react router dom](https://reactrouter.com/en/main)
+- [redux toolkit](https://redux-toolkit.js.org/)
+- [redux](https://redux.js.org/)
+- [react redux](https://react-redux.js.org/)
+
+## 🏎 Creating production build
+
+1. Create a production ready react app using the command :
+
+   ```javascript
+   npm run build
+   ```
+
+## ✨ Features
--- src/redux/features/cartSlice.js
+++ src/redux/features/cartSlice.js
@@ -3,9 +3,25 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   cartItems: [],
 };
+
+const CART_LOCAL_STORAGE_KEY = "cartItems";
+
+// Define a helper function to save the cartItems array in localStorage
+const saveCartItemsToLocalStorage = (cartItems) => {
+  localStorage.setItem(CART_LOCAL_STORAGE_KEY, JSON.stringify(cartItems));
+};
+
+// Define a helper function to retrieve the cartItems array from localStorage
+const getCartItemsFromLocalStorage = () => {
+  const cartItems = localStorage.getItem(CART_LOCAL_STORAGE_KEY);
+  return cartItems ? JSON.parse(cartItems) : [];
+};
+
 const cartSlice = createSlice({
   name: "cart",
-  initialState,
+  initialState: {
+    cartItems: getCartItemsFromLocalStorage(),
+  },
   reducers: {
     addToCart: (state, action) => {
       const itemInCart = state.cartItems.find(
@@ -16,10 +32,14 @@ const cartSlice = createSlice({
       } else {
         state.cartItems.push({ ...action.payload, quantity: 1 });
       }
+      // Save the updated cartItems array in localStorage
+      saveCartItemsToLocalStorage(state.cartItems);
     },
     incrementQuantity: (state, action) => {
       const item = state.cartItems.find((item) => item.id === action.payload);
       item.quantity++;
+      // Save the updated cartItems array in localStorage
+      saveCartItemsToLocalStorage(state.cartItems);
     },
     decrementQuantity: (state, action) => {
       const item = state.cartItems.find((item) => item.id === action.payload);
@@ -28,12 +48,16 @@ const cartSlice = createSlice({
       } else {
         item.quantity--;
       }
+      // Save the updated cartItems array in localStorage
+      saveCartItemsToLocalStorage(state.cartItems);
     },
     removeItem: (state, action) => {
       const removeItem = state.cartItems.filter(
         (item) => item.id !== action.payload
       );
       state.cartItems = removeItem;
+      // Save the updated cartItems array in localStorage
+      saveCartItemsToLocalStorage(state.cartItems);
     },
   },
 });
